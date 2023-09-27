3e3155fbca899444595bace1408eb1267f2ffdd0
IgorFelipe10 <113717418+IgorFelipe10@users.noreply.github.com>
2023-09-27 11:27:53 -0300

    Update paginaRanking.swift

--- projeto/projeto/paginaRanking.swift
+++ projeto/projeto/paginaRanking.swift
@@ -1,32 +1,60 @@
 import SwiftUI
 
+
+
+extension Color {
+    static let verdeAmarelo = Color("verdeAmarelo")
+}
+
 struct paginaRanking: View {
-    let verdeAmarelo = Color("verdeAmarelo")
     let verdeFundo = Color("verdeFundo")
     let botao = Color("botao")
-  
     
+
+    
+    @StateObject var vm = ViewModel()
+    
+    @State private var selectedAnimal: Animal? = nil
+
     var body: some View {
         ZStack {
             verdeFundo
                 .edgesIgnoringSafeArea(.all)
             
-            List {
-                Text("Aprenda mais sobre os animais!")
-                    .font(.largeTitle)
-                    .foregroundColor(Color.black)
-                    .padding()
+            VStack {
+                List(vm.animals) { animal in
+                    Button(action: {
+                       
+                        selectedAnimal = animal
+                    }) {
+                        Text(animal.nome!)
+                    }
+                }
+                .cornerRadius(10)
+                .padding()
                 
-                Text("A List Item 1")
-                Text("A Second List Item 2")
-                Text("A Third List Item 3")
+                if let selectedAnimal = selectedAnimal {
+                    Text("Nome: \(selectedAnimal.nome!)")
+                    Text("Nome Científico: \(selectedAnimal.nome_c!)")
+                    Text("Local: \(selectedAnimal.local!)")
+                    Text("Dieta: \(selectedAnimal.dieta!)")
+                    Text("Cor: \(selectedAnimal.cor!)")
+                    Text("Peso: \(selectedAnimal.peso!)")
+                    Text("Altura: \(selectedAnimal.altura!)")
+                    Text("Tempo de Vida: \(selectedAnimal.tempo_vida!)")
+                    Text("População: \(selectedAnimal.populacao!)")
+                    Text("Período: \(selectedAnimal.periodo!)")
+                }
             }
-            .background(Color.white)
-            .cornerRadius(10)
-            .padding()
+            .onAppear {
+                vm.fetchDataFromAPI()
+            }
         }
+       
     }
-}
+
+   
+}   
 
 struct paginaRanking_Previews: PreviewProvider {
     static var previews: some View {
