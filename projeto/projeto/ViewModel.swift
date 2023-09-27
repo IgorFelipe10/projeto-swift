a37eae15bab95503677be4d540e57e022cd20158
IgorFelipe10 <113717418+IgorFelipe10@users.noreply.github.com>
2023-09-27 11:23:24 -0300

    Add files via upload

---
+++ projeto/projeto/ViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  ViewModel.swift
+//  projeto
+//
+//  Created by Student11 on 27/09/23.
+//
+
+import Foundation
+struct Animal: Identifiable, Decodable {
+    let id  : Int?
+    let nome: String?
+    let nome_c: String?
+    let local: String?
+    let dieta: String?
+    let cor: String?
+    let peso: String?
+    let altura: String?
+    let tempo_vida: String?
+    let populacao: String?
+    let periodo: String?
+}
+
+
+
+class ViewModel : ObservableObject {
+    
+    @Published var animals: [Animal] = []
+    
+    func fetchDataFromAPI() {
+        guard let url = URL(string: "http://192.168.128.244:1880/abidleREAD") else {
+            return
+        }
+        
+        let task =  URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            
+            guard let data = data, error == nil else {return}
+            
+            do{
+                let animals = try JSONDecoder().decode([Animal].self, from: data)
+               
+                DispatchQueue.main.async {
+                    self?.animals = animals
+                }
+                
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    
+}
+
