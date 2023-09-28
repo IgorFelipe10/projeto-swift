f31143769666aeea9d0b27a1d1864b3a1f2026df
IgorFelipe10 <113717418+IgorFelipe10@users.noreply.github.com>
2023-09-28 12:37:18 -0300

    Update paginaRanking.swift

--- projeto/projeto/paginaRanking.swift
+++ projeto/projeto/paginaRanking.swift
@@ -1,63 +1,61 @@
 import SwiftUI
 
-
-
 extension Color {
     static let verdeAmarelo = Color("verdeAmarelo")
 }
 
 struct paginaRanking: View {
+    let verdeAmarelo = Color("verdeAmarelo")
     let verdeFundo = Color("verdeFundo")
-    let botao = Color("botao")
-    
-
-    
+    let marrom = Color("marrom")
     @StateObject var vm = ViewModel()
-    
     @State private var selectedAnimal: Animal? = nil
-
+    
     var body: some View {
-        ZStack {
-            verdeFundo
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                List(vm.animals) { animal in
-                    Button(action: {
-                       
-                        selectedAnimal = animal
-                    }) {
-                        Text(animal.nome!)
+        NavigationView{
+            ZStack {
+                verdeFundo
+                    .edgesIgnoringSafeArea(.all)
+                
+                ZStack {
+                    Rectangle()
+                        .frame(width: 300, height: 550)
+                        .foregroundColor(verdeAmarelo)
+                        .cornerRadius(20)
+                    VStack(alignment: .leading, spacing: 8) { 
+                        ForEach(vm.animals) { animal in
+                            NavigationLink(destination: detalheAnimal(selectedAnimal: animal)){
+                                Text(animal.nome!)
+                                    .font(.subheadline)
+                            }
+                        }
+                    }
+                    .cornerRadius(10)
+                    .padding()
+                    
+                    if let selectedAnimal = selectedAnimal {
+                        Text("Nome: \(selectedAnimal.nome!)")
+                        Text("Nome Científico: \(selectedAnimal.nomec!)")
+                        Text("Local: \(selectedAnimal.local!)")
+                        Text("Dieta: \(selectedAnimal.dieta!)")
+                        Text("Cor: \(selectedAnimal.cor!)")
+                        Text("Peso: \(selectedAnimal.peso!)")
+                        Text("Altura: \(selectedAnimal.altura!)")
+                        Text("Tempo de Vida: \(selectedAnimal.tempovida!)")
+                        Text("População: \(selectedAnimal.populacao!)")
+                        Text("Período: \(selectedAnimal.periodo!)")
                     }
                 }
-                .cornerRadius(10)
-                .padding()
-                
-                if let selectedAnimal = selectedAnimal {
-                    Text("Nome: \(selectedAnimal.nome!)")
-                    Text("Nome Científico: \(selectedAnimal.nome_c!)")
-                    Text("Local: \(selectedAnimal.local!)")
-                    Text("Dieta: \(selectedAnimal.dieta!)")
-                    Text("Cor: \(selectedAnimal.cor!)")
-                    Text("Peso: \(selectedAnimal.peso!)")
-                    Text("Altura: \(selectedAnimal.altura!)")
-                    Text("Tempo de Vida: \(selectedAnimal.tempo_vida!)")
-                    Text("População: \(selectedAnimal.populacao!)")
-                    Text("Período: \(selectedAnimal.periodo!)")
+                .onAppear {
+                    vm.fetchDataFromAPI()
                 }
             }
-            .onAppear {
-                vm.fetchDataFromAPI()
-            }
         }
-       
     }
-
-   
-}   
-
-struct paginaRanking_Previews: PreviewProvider {
-    static var previews: some View {
-        paginaRanking()
+    
+    struct paginaRanking_Previews: PreviewProvider {
+        static var previews: some View {
+            paginaRanking()
+        }
     }
 }
